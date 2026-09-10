4fc0c030cbaf88fb6386a146c6a9e93b40e861ab
StyleDeolg <ddeorg@gmail.com>
2026-09-10 03:08:23 +0300

    Улучшен LoadingScreen, добавлены новые персонажи

--- frontend/src/components/ShardIcon.tsx
+++ frontend/src/components/ShardIcon.tsx
@@ -7,7 +7,7 @@ interface ShardIconProps {
 export default function ShardIcon({ size = 24, className = '', style = {} }: ShardIconProps) {
     return (
         <img
-            src="/images/wheel/shard.png"
+            src="/images/wheel/shard1.png"
             alt="Осколок луны"
             className={className}
             style={{
